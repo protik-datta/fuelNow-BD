d9307d1134532a426cec793d4ea9372f22971b40
Protik Datta <protikdatta135@gmail.com>
2026-04-14 00:02:04 +0600

    server: basic setup done

---
+++ server/src/app.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const compression = require("compression");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const logger = require("./utils/logger");
+
+const app = express();
+
+// cors
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+// security
+app.use(helmet());
+app.use(hpp());
+app.use(mongoSanitize());
+
+// performance
+app.use(compression());
+
+// body parser
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// rate limit
+app.use(
+  "/api",
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+  }),
+);
+
+// logging
+app.use(
+  morgan("dev", {
+    stream: {
+      write: (msg) => logger.info(msg.trim()),
+    },
+  }),
+);
+
+// connect db
+const connectDB = require("./config/db.config");
+connectDB();
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "API Running 🚀" });
+});
+
+// -----------------------------------------------------
+
+
+const errorHandler = require("./middleware/errorHandler");
+app.use(errorHandler);
+
+module.exports = app;
